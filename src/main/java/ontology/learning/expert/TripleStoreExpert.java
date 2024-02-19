5da4ab0cceb3bb91cc03a61539cdbcaf53d82fa0
Baris Sertkaya <3862859+sertkaya@users.noreply.github.com>
2024-02-19 11:44:20 +0100

    Added SPARQL-query generation from existential restriction.

--- src/main/java/ontology/learning/expert/TripleStoreExpert.java
+++ src/main/java/ontology/learning/expert/TripleStoreExpert.java
@@ -7,9 +7,7 @@ import org.apache.logging.log4j.Logger;
 // import org.eclipse.rdf4j.repository.RepositoryConnection;
 // import org.eclipse.rdf4j.repository.RepositoryException;
 
-import org.semanticweb.owlapi.model.OWLClassExpression;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
+import org.semanticweb.owlapi.model.*;
 
 // import org.aksw.owl2sparql.OWLClassExpressionToSPARQLConverter;
 // import org.aksw.owl2sparql.OWLAxiomToSPARQLConverter;
@@ -107,14 +105,21 @@ public class TripleStoreExpert implements ExpertOracle {
 		
 	}
 	
-	public String buildWhereClause(int subject, OWLClassExpression c) {
-		String clause = "";
-		
+	public String buildWhereClause(int subject, OWLClassExpression c, int successorCount) {
+
+		String s = "?" + String.valueOf(subject);
 		switch (c.getClassExpressionType()) {
 		case OWL_CLASS:
-			return("?" + subject + " wdt:P31 " + c.toString());
+			return(s + " wdt:P31 " + c + ".\n");
 		case OBJECT_SOME_VALUES_FROM:
-			break;
+			String p = (((OWLObjectSomeValuesFrom) c).getProperty()).toString();
+			int object = 10 * subject + successorCount;
+			String o = "?" + String.valueOf(object);
+
+			// OWLClassExpression filler = ((OWLObjectSomeValuesFrom) c).getFiller();
+			// if (filler.getClassExpressionType().equals(ClassExpressionType.OBJECT_SOME_VALUES_FROM))
+			successorCount = 0;
+			return(s + " " + p + " " + o + ". \n" +  buildWhereClause(object, ((OWLObjectSomeValuesFrom) c).getFiller(), successorCount));
 		case DATA_ALL_VALUES_FROM:
 			break;
 		case DATA_EXACT_CARDINALITY:
@@ -138,7 +143,13 @@ public class TripleStoreExpert implements ExpertOracle {
 		case OBJECT_HAS_VALUE:
 			break;
 		case OBJECT_INTERSECTION_OF:
-			break;
+			String clause = "";
+			for (OWLClassExpression conjunct : c.asConjunctSet()) {
+				clause += buildWhereClause(subject, conjunct, successorCount);
+				if (conjunct.getClassExpressionType().equals(ClassExpressionType.OBJECT_SOME_VALUES_FROM))
+					++successorCount;
+			}
+			return(clause);
 		case OBJECT_MAX_CARDINALITY:
 			break;
 		case OBJECT_MIN_CARDINALITY:
@@ -150,7 +161,8 @@ public class TripleStoreExpert implements ExpertOracle {
 		default:
 			break;
 		}
-		return(clause);
+
+		return("xxx");
 	}
 	
 	
--- src/test/java/ontology/learning/test/TestTripleStoreExpert.java
+++ src/test/java/ontology/learning/test/TestTripleStoreExpert.java
@@ -8,10 +8,7 @@ import java.util.Set;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.OWLClassExpression;
-import org.semanticweb.owlapi.model.OWLDataFactory;
-import org.semanticweb.owlapi.model.OWLObjectProperty;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.*;
 
 import ontology.learning.expert.TripleStoreExpert;
 
@@ -30,17 +27,79 @@ public class TestTripleStoreExpert {
 		OWLClassExpression clsD = df.getOWLClass("http://www.semanticweb.org/bs/ontologies/2023/11/untitled-ontology-20#D");
 		OWLClassExpression thing = df.getOWLThing();
 		OWLObjectProperty propR = df.getOWLObjectProperty("r");
-	
+
+
+		// depth 1
+		// (some r A)
 		OWLClassExpression existsRA = df.getOWLObjectSomeValuesFrom(propR, clsA);
+		// (some r B)
 		OWLClassExpression existsRB = df.getOWLObjectSomeValuesFrom(propR, clsB);
-		
+		// (some r B)
+		OWLClassExpression existsRC = df.getOWLObjectSomeValuesFrom(propR, clsC);
+
+		Set<OWLClassExpression> tmp = new HashSet<>();
+		tmp.add(clsA);
+		tmp.add(clsB);
+		// (A and B)
+		OWLObjectIntersectionOf AB = df.getOWLObjectIntersectionOf(tmp);
+
+		// conjunction in depth 1
+		// (some r (A and B))
+		OWLClassExpression existsRAB = df.getOWLObjectSomeValuesFrom(propR, AB);
+
+		// depth 2
+		// (some r (some r A))
+		OWLClassExpression existsRRA = df.getOWLObjectSomeValuesFrom(propR, existsRA);
+
+		// conjunction in depth 2
+		// (some r (some r (A and B)))
+		OWLClassExpression existsRRAB = df.getOWLObjectSomeValuesFrom(propR, existsRAB);
+
+		// (some r (A and (some r B)))
+		tmp = new HashSet<>();
+		tmp.add(clsA);
+		tmp.add(existsRB);
+		OWLClassExpression existsRA_existsRB = df.getOWLObjectSomeValuesFrom(propR, df.getOWLObjectIntersectionOf(tmp));
+
+		tmp = new HashSet<>();
+		// (some r (A and (some r (A and B))))
+		tmp.add(clsA);
+		tmp.add(existsRAB);
+		OWLObjectIntersectionOf tmp_conjunction = df.getOWLObjectIntersectionOf(tmp);
+		OWLClassExpression existsRA_existsRAB = df.getOWLObjectSomeValuesFrom(propR, tmp_conjunction);
+
+		// (some r (A and ((some r A) and (some r B) and (some r C))))
+		tmp = new HashSet<>();
+		// inner conjunction
+		tmp.add(clsA);
+		tmp.add(existsRA);
+		tmp.add(existsRB);
+		tmp.add(existsRC);
+		tmp_conjunction = df.getOWLObjectIntersectionOf(tmp);
+		OWLClassExpression existsRA_existsRAexistsRABC = df.getOWLObjectSomeValuesFrom(propR, tmp_conjunction);
+
+		// (some r (A and ((some r A) and (some r B) and (some r C) and (some r (A and (some r B))))))
+		tmp.add(existsRA_existsRB);
+		tmp_conjunction = df.getOWLObjectIntersectionOf(tmp);
+		OWLClassExpression existsRA_existsRAexistsRABCexistsRA_existsRAB = df.getOWLObjectSomeValuesFrom(propR, tmp_conjunction);
+
 		Set<OWLClassExpression> baseSet = new HashSet<OWLClassExpression>();
+		// depth 0
 		baseSet.add(clsA);
 		baseSet.add(clsB);
-		baseSet.add(clsC);
-		// baseSet.add(existsRA);
-		// baseSet.add(existsRB);
-	
+		// baseSet.add(clsC);
+
+		// depth 1
+		baseSet.add(existsRA);
+		baseSet.add(existsRB);
+		baseSet.add(existsRAB);
+
+		// depth 2
+		// baseSet.add(existsRRA);
+		// baseSet.add(existsRRAB);
+
+		// depth 3
+		// baseSet.add(existsRA_existsRAexistsRABCexistsRA_existsRAB);
 		/*
 		URL kgURL = null;
 		try {
@@ -53,8 +112,9 @@ public class TestTripleStoreExpert {
 		*/
 		String kg = "/home/bs/research/dev/pacco/src/test/resources/eu-g7-members.ttl";
 		TripleStoreExpert expert = new TripleStoreExpert(kg);
-		System.out.println("query:" + expert.buildWhereClause(0, clsA));
-		
+		System.out.println(expert.buildWhereClause(1, df.getOWLObjectIntersectionOf(baseSet), 0));
+		// System.out.println("query:" + expert.buildWhereClause(0, clsC));
+
 		// OWLClassExpression lhs = df.getOWLObjectIntersectionOf(clsA);
 		// OWLClassExpression rhs = df.getOWLObjectIntersectionOf(clsB);
 		// OWLSubClassOfAxiom ax = df.getOWLSubClassOfAxiom(lhs, rhs);
